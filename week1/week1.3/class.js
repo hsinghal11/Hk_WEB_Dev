f3be51522a5231fc8422834ff39a4329cb6c56c7
Himanshu_Singhal <himanshusinghal11082004@gmail.com>
2023-12-23 12:24:09 +0530

    test added

---
+++ week1/week1.3/class.js
@@ -0,0 +1,12 @@
+class animal{
+    constructor(name,leg,speak){
+        this.name=name;
+        this.leg=leg;
+        this.speak=speak;
+    }
+}
+
+console.log(new animal("Doggy",4,"bhow bhow"));
+console.log(dog);
+console.log(dog.name);
+
